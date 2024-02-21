2476d171a8db1ade937aa138b4cc47ec65d3dcb0
neerajnerlekar <neeraj.nerlekar@gmail.com>
2024-02-21 12:25:39 -0700

    refactor max function and add 2 more test for second argument and equality

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,7 +1,29 @@
 import { describe, test, it, expect} from 'vitest'
+import { max } from '../src/intro'
 
 describe('Intro', () => {
-    it('should return the first argument if it is greater than the second', () => {
+    describe('max', () => {
+
+        it('should return the first argument if it is greater than the second', () => {
+            // // Arrange
+            // const a =  2;
+            // const b = 1;
+            
+            // // Act
+        // const result = max(a, b);
+
+        // // Assert
+        // expect(result).toBe(a);
+        // The above code is replaced by the below code
         expect(max(2, 1)).toBe(2);
-    }
-    );
+        });
+    
+        it('should return the second argument if it is greater than the first', () => {
+            expect(max(1, 2)).toBe(2);
+        });
+    
+        it('should return the first argument if arguments are equal', () => {
+            expect(max(1, 1)).toBe(1);
+        });
+    });
+});
